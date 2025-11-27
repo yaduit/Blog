32f0696a2bd62c4606f9367855e72cff415b47fb
yaduit <yk226411@gmail.com>
2025-11-27 10:17:42 +0530

    Ignore node_modules

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
+const path  = require('path');
 
-app.get("/", function(req,res){
-    res.send("Hello Buddy");
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname,'public')));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+app.get('/',function(req,res){
+    res.render('register');
 })
 
+
+
 app.listen(3000,function(){
-    console.log("server is running...")
+    console.log("server is running on 3000...")
 })
